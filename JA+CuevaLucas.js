acdf21ee98fea517764e252c061f63db428dd94b
Lucas Maximiliano Cueva <lucasmaximiliano@outlook.com>
2021-03-26 17:03:52 -0300

    Delete JA+CuevaLucas.js

--- JA+CuevaLucas.js
+++
@@ -1,9 +0,0 @@
-const sampleText1 =
-  "TypeScript is an open-source language which builds on JavaScript, one of the world’s most used tools, by adding static type definitions.";
-
-const sampleText2 =
-  "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.";
-
-const sampleText3 =
-  "With MongoDB Atlas, your self-healing clusters are made up of geographically distributed database instances to ensure no single point of failure. ";
-
