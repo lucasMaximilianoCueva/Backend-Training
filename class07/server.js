01bdf96a67201980506e118a8d72acee62da59ae
Lucas Maximiliano Cueva <lucasmaximiliano@outlook.com>
2021-04-16 16:34:24 -0300

    update server.js

--- class07/server.js
+++ class07/server.js
@@ -1,53 +1,36 @@
-import express from 'express'
+import express from "express";
+import fs from "fs";
 
-const app = express()
+const app = express();
 
 let visitItem = 0;
 let visitItems = 0;
-const items = [
-    {
-      "id": 1,
-      "title": "Mercedes Benz C Class",
-      "price": 48000,
-      "thumbnail": "https://i.blogs.es/4ba188/mercedes-benz-c-class-2019-1600-01/1366_2000.jpg"
-    },
-    {
-      "id": 2,
-      "title": "BMW Serie 3",
-      "price": 45000,
-      "thumbnail": "https://hips.hearstapps.com/es.h-cdn.co/cades/contenidos/9888/bmw-serie-3-2018-delantera.jpg?resize=980:*"
-    }
-  ]
 
-app.get('/items', (req,res) => {
-    ++visitItems
-    res.json(`{items: ${JSON.stringify(items)}, quantity: ${items.length}}`)
-})
+app.get("/items", (req, res) => {
+  ++visitItems;
+  const items = JSON.parse(fs.readFileSync("./files/products.txt", "utf-8"));
+  const quantity = items.length;
+  res.json({ items, quantity });
+});
 
-app.get('/item-random', (req,res) => {
-    ++visitItem
-    res.json(JSON.stringify(items[Math.floor(Math.random() * items.length)]))
-})
+app.get("/item-random", (req, res) => {
+  ++visitItem;
+  const items = JSON.parse(fs.readFileSync("./files/products.txt", "utf-8"));
+  const quantity = items.length;
+  res.json({
+    item: items[Math.floor(Math.random() * quantity)],
+  });
+});
 
-app.get('/visits', (req,res) => {
-    res.send(`Visits on "/items": ${visitItems} \t Visits on "/random-item": ${visitItem}`)
-})
+app.get("/visits", (req, res) => {
+  res.send(
+    `Visits on "/items": ${visitItems} \t Visits on "/random-item": ${visitItem}`
+  );
+});
 
-const PORT = 8080
+const PORT = 8080;
 
 const server = app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${server.address().port}`)
-})
-server.on("error", error => console.log(`Error on Server ${error}`))
-
-
-// import express from 'express';
-
-// const app = express();
-
-// const port = 8080;
-// const server = app.listen(port, () => {
-//     console.log(`Running server on PORT: ${server.address().port}`)
-// });
-
-// server.on('error', err => console.log('ERROR: ' + err));
+  console.log(`Server listening on PORT ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error on Server ${error}`));
