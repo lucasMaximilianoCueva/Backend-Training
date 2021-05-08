c7178ac618c51b26b29d21ba041894f7541214bc
Lucas Maximiliano Cueva <lucasmaximiliano@outlook.com>
2021-05-08 16:39:39 -0300

    Add files via upload

---
+++ class13/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import { Server as HttpServer } from 'http';
+import { Server as Socket } from 'socket.io';
+import { createFront } from './routes/front.js';
+import { createProducts } from './routes/products.js';
+import { products } from './prod.js';
+
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new Socket(httpServer);
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: 'index.hbs',
+  }));
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use('/', createFront());
+app.use('/api/products', createProducts());
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const messages = [
+  {author: "juan@gmail.com", text: "Hey! How are you?"},
+  {author: "pedro@outlook.com", text: "Great! and you?"},
+  {author: "lena@icloud.com", text: "Excelent!"}
+]
+
+io.on('connection', socket => {
+  console.log('client connected!');
+  socket.emit('products', { products: products.getProd() });
+
+  socket.on('btn', () => {
+    socket.emit('products', { products: products.getProd() });
+  });
+
+  socket.emit('messages', messages);
+
+    socket.on('new-message', data => {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+});
+
+const PORT = 8080;
+
+const server = httpServer.listen(PORT, () => {
+  console.log(`Http server listening on port ${server.address().port}`);
+});
+
+server.on("error", error => console.log(`Error: ${error}`));
