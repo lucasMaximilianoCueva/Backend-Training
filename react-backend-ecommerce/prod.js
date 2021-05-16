2ce8b03924bffd72d5cabc4b14d1896df75cf00d
Lucas Maximiliano Cueva <lucasmaximiliano@outlook.com>
2021-05-16 03:45:46 -0300

    Update prod.js

--- react-backend-ecommerce/prod.js
+++ react-backend-ecommerce/prod.js
@@ -1,6 +1,17 @@
 export class ProductsDB {
     constructor() {
-        this.PRODUCTS_DB = [];
+        this.PRODUCTS_DB = [
+             {
+                    "title": "Volkswagen",
+                    "timestamp": "2021-06-13T17:39:53.231Z",
+                    "code": 178221,
+                    "description": "Electric car",
+                    "price": 80000,
+                    "thumbnail": "https://s7d1.scene7.com/is/image/volkswagen/VW_NGW6_Launch_ID4_Homepage_Masthead?Zml0PWNyb3AsMSZmbXQ9anBnJnFsdD03OSZ3aWQ9ODAwJmhlaT04MDAmYWxpZ249MC4wMCwwLjAwJmEzZWU=",
+                    "id": 1,
+                    "stock": 100
+                }
+        ];
         this.nextProdDb = 0;
     }
 
@@ -51,43 +62,3 @@ const prod = new ProductsDB();
 export { prod };
 
 
-// {
-//     "title": "Volkswagen",
-//     "timestamp": "2021-06-13T17:39:53.231Z",
-//     "code": 178221,
-//     "description": "Electric car",
-//     "price": 80000,
-//     "thumbnail": "https://s7d1.scene7.com/is/image/volkswagen/VW_NGW6_Launch_ID4_Homepage_Masthead?Zml0PWNyb3AsMSZmbXQ9anBnJnFsdD03OSZ3aWQ9ODAwJmhlaT04MDAmYWxpZ249MC4wMCwwLjAwJmEzZWU=",
-//     "id": 1,
-//     "stock": 100
-// },
-// {
-//   "title": "Volkswagen",
-//     "timestamp": "2021-06-13T17:39:53.231Z",
-//     "code": 178221,
-//     "description": "Electric car",
-//     "price": 80000,
-//     "thumbnail": "https://s7d1.scene7.com/is/image/volkswagen/VW_NGW6_Launch_ID4_Homepage_Masthead?Zml0PWNyb3AsMSZmbXQ9anBnJnFsdD03OSZ3aWQ9ODAwJmhlaT04MDAmYWxpZ249MC4wMCwwLjAwJmEzZWU=",
-//     "id": 2,
-//     "stock": 100
-// },
-// {
-// "title": "Volkswagen",
-//     "timestamp": "2021-06-13T17:39:53.231Z",
-//     "code": 178221,
-//     "description": "Electric car",
-//     "price": 80000,
-//     "thumbnail": "https://s7d1.scene7.com/is/image/volkswagen/VW_NGW6_Launch_ID4_Homepage_Masthead?Zml0PWNyb3AsMSZmbXQ9anBnJnFsdD03OSZ3aWQ9ODAwJmhlaT04MDAmYWxpZ249MC4wMCwwLjAwJmEzZWU=",
-//     "id": 3,
-//     "stock": 100
-// },
-// {
-// "title": "Volkswagen",
-//     "timestamp": "2021-06-13T17:39:53.231Z",
-//     "code": 178221,
-//     "description": "Electric car",
-//     "price": 80000,
-//     "thumbnail": "https://s7d1.scene7.com/is/image/volkswagen/VW_NGW6_Launch_ID4_Homepage_Masthead?Zml0PWNyb3AsMSZmbXQ9anBnJnFsdD03OSZ3aWQ9ODAwJmhlaT04MDAmYWxpZ249MC4wMCwwLjAwJmEzZWU=",
-//     "id": 4,
-//     "stock": 100
-// }
